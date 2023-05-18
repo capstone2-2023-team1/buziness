062f5c24ba75a53e8efbd90043bfd2df22d24f67
ysndy <ysndy@naver.com>
2023-05-18 18:11:51 +0900

    1차 시연 준비 완료

--- src/main/java/com/example/nerfbiz/Constant.java
+++ src/main/java/com/example/nerfbiz/Constant.java
@@ -2,6 +2,10 @@ package com.example.nerfbiz;
 
 public interface Constant {
 
-    String FILE_PATH = "C:/Riot Games/";
-
+    String FILE_PATH_RESOURCES = System.getProperty("user.dir")+"\\src\\main\\resources\\";
+    String FILE_PATH_STATIC = FILE_PATH_RESOURCES+"static\\";
+    String FILE_PATH = FILE_PATH_STATIC+"videos\\";
+    String FRONT_SERVER_PATH = "";
+    String FUNCTIONAL_SERVER_PATH = "http://127.0.0.1:8000/";
+    String FUNCTIONAL_SERVER_PATH_VIDEO2TRD = FUNCTIONAL_SERVER_PATH+"trans/";
 }
--- src/main/java/com/example/nerfbiz/VideoController.java
+++ src/main/java/com/example/nerfbiz/VideoController.java
@@ -1,19 +1,34 @@
 package com.example.nerfbiz;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+import com.google.cloud.storage.BlobId;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 public class VideoController {
 
     @RequestMapping(value="video", method= RequestMethod.POST)
@@ -24,6 +39,7 @@ public class VideoController {
         System.out.println("paramMap =>"+files[0]);
 
         String filepath = Constant.FILE_PATH;
+        System.out.println(filepath);
         for (MultipartFile mf : files) {
 
             String originFileName = mf.getOriginalFilename(); // 원본 파일 명
@@ -31,22 +47,54 @@ public class VideoController {
             System.out.println("originFileName : " + originFileName);
             System.out.println("fileSize : " + fileSize);
 
-            String safeFile =System.currentTimeMillis() + originFileName;
+            String safeFile =System.currentTimeMillis()+"";
 
             FileNames = FileNames+","+safeFile;
             try {
-                File f1 = new File(filepath+safeFile);
+
+                String pathname = filepath+safeFile;
+                File f1 = new File(pathname);
                 mf.transferTo(f1);
-            } catch (IllegalStateException e) {
-                e.printStackTrace();
+
+                //credential 객체 생성
+                String credentialsPath = Constant.FILE_PATH_RESOURCES+"protean-pager-386913-984d487862d2.json";
+                GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsPath));
+
+                //storage 객체 생성
+                Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+                Path path = Paths.get(pathname);
+                byte[] content = Files.readAllBytes(path);
+
+                //cloud에 영상 전송
+                BlobId blobId = BlobId.of("nerf-video", "videos/"+safeFile);
+                BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("video/mp4").build();
+                storage.create(blobInfo, content);
+
+                //NeRF-server에 작업 요청
+                RestTemplate restTemplate = new RestTemplate();
+                String apiUrl = Constant.FUNCTIONAL_SERVER_PATH_VIDEO2TRD+"?video=https://storage.googleapis.com/nerf-video/videos/"+safeFile+"&identifier="+safeFile;
+                HttpHeaders headers = new HttpHeaders();
+                String requestBody = "{\"video\": \"https://storage.googleapis.com/nerf-video/"+safeFile+"\", \"identifier\": \""+safeFile+"\"}"; //영상주소 전달
+                HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
+                ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.GET, requestEntity, String.class);
+                String responseBody = response.getBody();
+                System.out.println(responseBody);
+
             } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+
+            catch (IllegalStateException e) {
                 e.printStackTrace();
             }
         }
         Map<String, Object> paramMap = new HashMap<String, Object>();
         FileNames = FileNames.substring(1);
+
         System.out.println("FileNames =>"+ FileNames);
         resultMap.put("JavaData", paramMap);
+        resultMap.put("obj_url", "https://storage.googleapis.com/nerf-video/obj/mesh.obj");
         return resultMap;
     }
 
