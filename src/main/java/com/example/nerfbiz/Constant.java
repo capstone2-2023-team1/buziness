9107c4fe6c57c5fda57ad01a116603525fd03527
ysndy <ysndy@naver.com>
2023-05-18 21:04:19 +0900

    file path 수정

--- src/main/java/com/example/nerfbiz/Constant.java
+++ src/main/java/com/example/nerfbiz/Constant.java
@@ -3,8 +3,7 @@ package com.example.nerfbiz;
 public interface Constant {
 
     String FILE_PATH_RESOURCES = "~/NeRF-biz/resources/";
-    String FILE_PATH_STATIC = FILE_PATH_RESOURCES+"static\\";
-    String FILE_PATH = FILE_PATH_STATIC+"videos\\";
+    String FILE_PATH = FILE_PATH_RESOURCES+"videos/";
     String FRONT_SERVER_PATH = "";
     String FUNCTIONAL_SERVER_PATH = "https://34.68.148.59:8000/";
     String FUNCTIONAL_SERVER_PATH_VIDEO2TRD = FUNCTIONAL_SERVER_PATH+"trans/";
