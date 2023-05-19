8d7af55e71ca67faef011a37358f64c7dac89ac9
ysndy <ysndy@naver.com>
2023-05-19 12:23:09 +0900

    기능서버 주소 수정

--- src/main/java/com/example/nerfbiz/Constant.java
+++ src/main/java/com/example/nerfbiz/Constant.java
@@ -5,7 +5,7 @@ public interface Constant {
     String FILE_PATH_RESOURCES = "classes/";
     String FILE_PATH = "classes/";
     String FRONT_SERVER_PATH = "";
-    String FUNCTIONAL_SERVER_PATH = "https://34.68.148.59:8000/";
+    String FUNCTIONAL_SERVER_PATH = "https://35.234.248.39:8000/";
     String FUNCTIONAL_SERVER_PATH_VIDEO2TRD = FUNCTIONAL_SERVER_PATH+"trans/";
     String RENDERING_SERVER_PATH = "https://35.223.2.97:3000/";
 }
